04bcc3cfc3a3a7588e723798ed42ad7144c22d5e
Umitduru03 <105162552+Umitduru03@users.noreply.github.com>
2022-06-07 14:17:03 +0300

    Add files via upload

---
+++ reload.py
@@ -0,0 +1,4 @@
+# Crocodile_bot
+Telegram bot for game "Crocodile"
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/MacroKeke/sessizSinema)
